7844c328315bba6df75dafb39cbb988d7f15be36
DESKTOP-QEMO99R\KIKim <daniel@fxgear.net>
2021-04-20 18:21:14 +0900

    health check

---
+++ RedisDemo/HealthChecks/ResponseTimeHealthCheck.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RedisDemo.HealthChecks
+{
+    public class ResponseTimeHealthCheck : IHealthCheck
+    {
+        private Random rnd = new Random();
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            int responseTimeInMS = rnd.Next(1, 300);
+            if(responseTimeInMS<100)
+            {
+                return Task.FromResult(HealthCheckResult.Healthy($"the response time looks good ({responseTimeInMS})."));
+            }
+            else if(responseTimeInMS <200)
+            {
+                return Task.FromResult(HealthCheckResult.Degraded($"the response time bit slow ({responseTimeInMS})."));
+            }
+            else
+            {
+                return Task.FromResult(HealthCheckResult.Unhealthy($"the response time is unacceptable ({responseTimeInMS})."));
+            }
+        }
+    }
+
+}
--- RedisDemo/Startup.cs
+++ RedisDemo/Startup.cs
@@ -1,11 +1,15 @@
+using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 using RedisDemo.Data;
+using RedisDemo.HealthChecks;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,13 +31,27 @@ namespace RedisDemo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddHealthChecksUI().AddInMemoryStorage();
+
             services.AddServerSideBlazor();
+            //configure health checks
+            services.AddHealthChecks()
+                .AddCheck<ResponseTimeHealthCheck>("Network speed test",null,new[] { "service" })
+            //    .AddCheck("foo service", () =>
+            //{
+            //    return HealthCheckResult.Degraded("서비스 작동안함");
+            //},new[] {"searvice"}
+            //)
+            //.AddCheck("bar service", () => HealthCheckResult.Healthy("서비스 작동"))
+            .AddCheck("Database", () => HealthCheckResult.Healthy("서비스 작동"),new[] {"database", "sql" });
+
             services.AddSingleton<WeatherForecastService>();
             services.AddStackExchangeRedisCache(options =>
             {
                 options.Configuration = Configuration.GetConnectionString("Redis");
                 options.InstanceName = "RedisDemo_";
             });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -57,9 +75,24 @@ namespace RedisDemo
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapHealthChecks("quickhealth", new HealthCheckOptions()
+                {
+                    Predicate = _ => false
+                });
+                endpoints.MapHealthChecks("/health/services", new HealthCheckOptions()
+                { 
+                  Predicate=reg =>reg.Tags.Contains("service"),
+                  ResponseWriter =UIResponseWriter.WriteHealthCheckUIResponse
+                });
+                endpoints.MapHealthChecks("/health",new HealthCheckOptions() {
+                ResponseWriter =UIResponseWriter.WriteHealthCheckUIResponse
+                });
                 endpoints.MapBlazorHub();
+                endpoints.MapHealthChecksUI();
                 endpoints.MapFallbackToPage("/_Host");
             });
+
+             
         }
     }
 }
